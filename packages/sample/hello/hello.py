05395d70eb9418e20c8ff63332799933e690c2e3
Padma rathi <padmarathi199@gmail.com>
2023-02-16 03:32:05 +0530

    Update hello.py

--- packages/sample/hello/hello.py
+++ packages/sample/hello/hello.py
@@ -1,6 +1,12 @@
+# def tessttt():
+#     var = "Hello"
+#     print(var)
+#     return var
+# tessttt()
+
+
 def main(args):
-      name = args.get("name", "stranger")
-      greeting = "Hello " + name + "!"
-      print(greeting)
-      return {"body": greeting}
-  
+    name = args.get("name", "stranger")
+    greeting = name
+    print(greeting)
+    return {"body": greeting}
